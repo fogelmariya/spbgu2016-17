25632d830eac4a30b5a58960d81ab121dc3616dc
fogelmariya <fogelmariya@gmail.com>
2016-11-29 01:28:36 +0300

    Add files via upload

---
+++ Fogel-Maria/DemoServlet1/src/main/java/com/google/DemoServlet.java
@@ -0,0 +1,17 @@
+package com.google;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Маруся on 27.11.2016.
+ */
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+}
